ff6deac823ec7d9e85c9b5ba8c0cdb4dd0a67609
kazuki.hirayama <kazuki.hirayama@rakus.co.jp>
2017-06-14 10:23:43 +0900

    プロジェクトを作成.

---
+++ src/main/java/com/example/demo/GitSampleHirayamaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleHirayamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleHirayamaApplication.class, args);
+	}
+}
